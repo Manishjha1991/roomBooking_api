37d583db5e4414bbbf5fdca33fd1968ba2460fd9
Manish <manish@Manishs-MacBook-Pro.local>
2017-10-05 01:37:39 +0530

    roomBooking Api

--- server.js
+++ server.js
@@ -1,11 +1,10 @@
 var express    = require("express");
 var user = require('./routes/user');
-var rooms = require('./routes/bookingRoom');
+var rooms = require('./routes/room');
 var bodyParser = require('body-parser');
 var app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
- 
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
@@ -18,7 +17,7 @@ router.get('/', function(req, res) {
     res.json({ message: 'welcome to our room booking apis' });
 });
  
-//route to handle user registration
+//route to handle user,room and more
 router.post('/users',user.register);
 router.post('/login',user.login);
 router.get('/rooms',rooms.getAllRooms)
