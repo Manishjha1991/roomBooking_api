10efb8e957573042a9c872cbe5ecb289ec91b718
Manish <manish@Manishs-MacBook-Pro.local>
2017-11-09 17:23:45 +0530

    add mongo atals url

--- db.js
+++ db.js
@@ -1,3 +1,3 @@
 module.exports = {
-    'url': 'mongodb://localhost:27017/db_userRegistration'
+    'url': 'mongodb://admin:developer@xelpmoc@cluster0-shard-00-00-ecc8q.mongodb.net:27017,cluster0-shard-00-01-ecc8q.mongodb.net:27017,cluster0-shard-00-02-ecc8q.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin'
 };
