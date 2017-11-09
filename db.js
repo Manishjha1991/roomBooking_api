9c3a9477f7e80d6120e2b77740e1b5d617e465c6
Manish <manish@Manishs-MacBook-Pro.local>
2017-11-09 17:33:02 +0530

    change name of db password

--- db.js
+++ db.js
@@ -1,3 +1,3 @@
 module.exports = {
-    'url': 'mongodb://admin:developer@xelpmoc@cluster0-shard-00-00-ecc8q.mongodb.net:27017,cluster0-shard-00-01-ecc8q.mongodb.net:27017,cluster0-shard-00-02-ecc8q.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin'
+    'url': 'mongodb://admin:xelpmocdeveloper@cluster0-shard-00-00-ecc8q.mongodb.net:27017,cluster0-shard-00-01-ecc8q.mongodb.net:27017,cluster0-shard-00-02-ecc8q.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin'
 };
